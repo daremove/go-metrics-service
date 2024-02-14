06a04bed610eccb0d6bd206764a3c3fc425239cb
daremove <daremove1@gmail.com>
2024-02-14 23:44:29 +0600

    Added fixes

--- internal/storage/database/database.go
+++ internal/storage/database/database.go
@@ -2,11 +2,10 @@ package database
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"github.com/daremove/go-metrics-service/internal/storage"
 	_ "github.com/golang-migrate/migrate/source/file"
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5"
 	"time"
 )
 
@@ -28,14 +27,14 @@ const (
 )
 
 type Database struct {
-	db *sql.DB
+	db *pgx.Conn
 }
 
-func checkConnection(ctx context.Context, db *sql.DB) error {
+func checkConnection(ctx context.Context, db *pgx.Conn) error {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
 	defer cancel()
 
-	if err := db.PingContext(ctx); err != nil {
+	if err := db.Ping(ctx); err != nil {
 		return err
 	}
 
@@ -49,10 +48,10 @@ func (d *Database) Ping(ctx context.Context) error {
 func (d *Database) GetGaugeMetric(ctx context.Context, key string) (storage.GaugeMetric, error) {
 	var result float64
 
-	row := d.db.QueryRowContext(ctx, "SELECT value FROM gauge_metrics WHERE id = $1", key)
+	row := d.db.QueryRow(ctx, "SELECT value FROM gauge_metrics WHERE id = $1", key)
 
 	if err := row.Scan(&result); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return storage.GaugeMetric{}, storage.ErrDataNotFound
 		}
 
@@ -65,7 +64,7 @@ func (d *Database) GetGaugeMetric(ctx context.Context, key string) (storage.Gaug
 func (d *Database) GetGaugeMetrics(ctx context.Context) ([]storage.GaugeMetric, error) {
 	var result []storage.GaugeMetric
 
-	rows, err := d.db.QueryContext(ctx, "SELECT * FROM gauge_metrics")
+	rows, err := d.db.Query(ctx, "SELECT id, value FROM gauge_metrics")
 
 	if err != nil {
 		return []storage.GaugeMetric{}, err
@@ -93,10 +92,10 @@ func (d *Database) GetGaugeMetrics(ctx context.Context) ([]storage.GaugeMetric, 
 func (d *Database) GetCounterMetric(ctx context.Context, key string) (storage.CounterMetric, error) {
 	var result int64
 
-	row := d.db.QueryRowContext(ctx, "SELECT value FROM counter_metrics WHERE id = $1", key)
+	row := d.db.QueryRow(ctx, "SELECT value FROM counter_metrics WHERE id = $1", key)
 
 	if err := row.Scan(&result); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		if errors.Is(err, pgx.ErrNoRows) {
 			return storage.CounterMetric{}, storage.ErrDataNotFound
 		}
 
@@ -109,7 +108,7 @@ func (d *Database) GetCounterMetric(ctx context.Context, key string) (storage.Co
 func (d *Database) GetCounterMetrics(ctx context.Context) ([]storage.CounterMetric, error) {
 	var result []storage.CounterMetric
 
-	rows, err := d.db.QueryContext(ctx, "SELECT * FROM counter_metrics")
+	rows, err := d.db.Query(ctx, "SELECT id, value FROM counter_metrics")
 
 	if err != nil {
 		return []storage.CounterMetric{}, err
@@ -135,7 +134,7 @@ func (d *Database) GetCounterMetrics(ctx context.Context) ([]storage.CounterMetr
 }
 
 func (d *Database) AddGaugeMetric(ctx context.Context, key string, value float64) error {
-	if _, err := d.db.ExecContext(ctx, InsertGaugeMetricQuery, key, value); err != nil {
+	if _, err := d.db.Exec(ctx, InsertGaugeMetricQuery, key, value); err != nil {
 		return err
 	}
 
@@ -143,7 +142,7 @@ func (d *Database) AddGaugeMetric(ctx context.Context, key string, value float64
 }
 
 func (d *Database) AddCounterMetric(ctx context.Context, key string, value int64) error {
-	if _, err := d.db.ExecContext(ctx, InsertCounterMetricQuery, key, value); err != nil {
+	if _, err := d.db.Exec(ctx, InsertCounterMetricQuery, key, value); err != nil {
 		return err
 	}
 
@@ -151,43 +150,31 @@ func (d *Database) AddCounterMetric(ctx context.Context, key string, value int64
 }
 
 func (d *Database) AddMetrics(ctx context.Context, gaugeMetrics []storage.GaugeMetric, counterMetrics []storage.CounterMetric) error {
-	tx, err := d.db.BeginTx(ctx, nil)
+	tx, err := d.db.BeginTx(ctx, pgx.TxOptions{})
 
 	if err != nil {
 		return err
 	}
 
-	defer tx.Rollback()
-
-	insertGaugeMetricStmt, err := tx.PrepareContext(ctx, InsertGaugeMetricQuery)
-
-	if err != nil {
-		return err
-	}
-
-	insertCounterMetricStmt, err := tx.PrepareContext(ctx, InsertCounterMetricQuery)
-
-	if err != nil {
-		return err
-	}
+	defer tx.Rollback(ctx)
 
 	for _, gaugeMetric := range gaugeMetrics {
-		if _, err := insertGaugeMetricStmt.ExecContext(ctx, gaugeMetric.Name, gaugeMetric.Value); err != nil {
+		if _, err := tx.Exec(ctx, InsertGaugeMetricQuery, gaugeMetric.Name, gaugeMetric.Value); err != nil {
 			return err
 		}
 	}
 
 	for _, counterMetric := range counterMetrics {
-		if _, err := insertCounterMetricStmt.ExecContext(ctx, counterMetric.Name, counterMetric.Value); err != nil {
+		if _, err := tx.Exec(ctx, InsertCounterMetricQuery, counterMetric.Name, counterMetric.Value); err != nil {
 			return err
 		}
 	}
 
-	return tx.Commit()
+	return tx.Commit(ctx)
 }
 
 func New(ctx context.Context, dsn string) (*Database, error) {
-	db, err := sql.Open("postgres", dsn)
+	db, err := pgx.Connect(ctx, dsn)
 
 	if err != nil {
 		return nil, err
@@ -197,15 +184,15 @@ func New(ctx context.Context, dsn string) (*Database, error) {
 		return nil, err
 	}
 
-	if _, err := db.ExecContext(ctx, `
+	if _, err := db.Exec(ctx, `
 		CREATE TABLE IF NOT EXISTS gauge_metrics (
-		    id      varchar PRIMARY KEY,
-    		value   double precision NOT NULL
+		    id      TEXT PRIMARY KEY,
+    		value   DOUBLE PRECISION NOT NULL
 		);
 
 		CREATE TABLE IF NOT EXISTS counter_metrics (
-			id      varchar PRIMARY KEY,
-			value   bigserial NOT NULL
+			id      TEXT PRIMARY KEY,
+			value   BIGSERIAL NOT NULL
 		);
 	`); err != nil {
 		return nil, err
